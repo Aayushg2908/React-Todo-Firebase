61d48ab241457abad8e1817fed0d27b1f523bad1
Aayush <aayushg2908@gmail.com>
2023-06-30 17:20:51 +0530

    React Todo app using firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
+// import localforage from "localforage";
 
-function App() {
+function App({db}) {
+  useEffect(()=>{
+    // localforage.getItem('tasks', (err, tasks)=>{
+    //   if(tasks){
+    //     setTasks(tasks);
+    //   }
+    // })
+    db.collection('tasks').doc('first').get().then((doc)=>{
+      if(doc.data().tasks){
+        setTasks(doc.data().tasks);
+      }
+    })
+  }, [])
+  const [task, setTask] = useState({ name: "", completed: false });
+  const [tasks, setTasks] = useState([]);
+  const changeHandler = (event) => {
+    setTask(event.target.value);
+  };
+  const storeTasks = (tasks)=>{
+    setTasks(tasks);
+    // localforage.setItem('tasks', tasks, (err)=>{
+    //   console.log("tasks saved")
+    // })
+    db.collection('tasks').doc('first').set({
+      tasks: tasks
+    })
+  }
+  const addTask = (t) => {
+    if (task.length !== 0) {
+      const newTasks = [...tasks];
+      newTasks.push({ name: t, completed: false });
+      storeTasks(newTasks);
+      setTask({ name: "", completed: false });
+    } else {
+      alert("Please enter valid input, Input can not be empty!!");
+    }
+  };
+  const updateHandler = (i) => {
+    const newTasks = [...tasks];
+    newTasks.splice(i, 1, {
+      name: newTasks[i].name,
+      completed: !newTasks[i].completed,
+    });
+    storeTasks(newTasks);
+  };
+  const deleteHandler = (i) => {
+    const newTasks = [...tasks];
+    newTasks.splice(i, 1);
+    storeTasks(newTasks);
+  };
+  const getTasks = () => {
+    return tasks.map((task, index) => (
+      <li
+        key = {index}
+        className={
+          task.completed
+            ? "list-group-item list-group-item-info user-select-none"
+            : "list-group-item list-group-item-danger user-select-none"
+        }
+        onDoubleClick={() => deleteHandler(index)}
+        onClick={() => updateHandler(index)}
+      >
+        {task.name}
+      </li>
+    ));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="my-3 text-primary">Welcome to Todo App</h1>
+      <div className="container my-4">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter your task"
+          onChange={changeHandler}
+          value={task.name}
+        />
+        <button className="btn btn-primary my-3" onClick={() => addTask(task)}>
+          Add Work
+        </button>
+        <ul className="list-group">{getTasks()}</ul>
+      </div>
+      <div className="container my-4">
+        <h4 className="text-danger user-select-none">
+          Red indicates that the work is still pending!!
+        </h4>
+        <h4 className="text-info user-select-none">
+          Blue indicates that the work is completed!!
+        </h4>
+      </div>
+      <div className="container">
+        <h5 className="user-select-none">Single click to mark the task as completed. Double click to delete the task.</h5>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+   apiKey: process.env.REACT_APP_API_KEY,
+   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+   projectId: process.env.REACT_APP_PROJECT_ID,
+   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+   appId: process.env.REACT_APP_APP_ID,
+   measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// Use these for db & auth
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App db={db}/>
   </React.StrictMode>
 );
 
